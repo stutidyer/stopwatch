427b23ebb7976ae099d375794f162fb678662cdd
unknown <stutidyer22@gmail.com>
2019-01-17 10:33:13 +0530

    css,js and html modified still lap is not working

--- js/app.js
+++ js/app.js
@@ -1,49 +1,96 @@
-var h1 = document.getElementsByTagName("h1")[0],
-  start = document.getElementById("start"),
-  stop = document.getElementById("stop"),
-  clear = document.getElementById("clear"),
-  seconds = 0,
-  minutes = 0,
-  hours = 0,
-  t;
+var startBtn = document.getElementById("start"),
+  stopBtn = document.getElementById("stop"),
+  lapBtn = document.getElementById("lap"),
+  clearBtn = document.getElementById("clear"),
+  running = false,
+  times = [],
+  display = document.querySelector(".stopwatch"),
+  results = document.getElementById("results"),
+  laps = [];
+eventListener();
 
-function add() {
-  seconds++;
-  if (seconds >= 60) {
-    seconds = 0;
-    minutes++;
-    if (minutes >= 60) {
-      minutes = 0;
-      hours++;
-    }
+function eventListener() {
+  document.addEventListener("DOMContentLoaded", print);
+  document.addEventListener("DOMContentLoaded", reset);
+  //  document.addEventListener("DOMContentLoaded", stopwatchs);
+  document.addEventListener("DOMContentLoaded", result);
+  startBtn.addEventListener("click", start());
+  stopBtn.addEventListener("click", stop());
+  lapBtn.addEventListener("click", lap());
+  clearBtn.addEventListener("click", clear());
+}
+
+function result() {
+  this.results = results;
+}
+
+function start() {
+  if (!this.time) this.time = performance.now();
+  if (!this.running) {
+    this.running = true;
+    requestAnimationFrame(this.step.bind(this));
   }
-
-  h1.textContent =
-    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
-    ":" +
-    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
-    ":" +
-    (seconds > 9 ? seconds : "0" + seconds);
-
-  timer();
 }
-function timer() {
-  t = setTimeout(add, 1000);
+function reset() {
+  this.times = [0, 0, 0];
 }
-timer();
+function stop() {
+  this.running = false;
+  this.time = null;
+}
 
-/* Start button */
-start.onclick = timer;
+function lap() {
+  let times = this.times;
+  let li = document.createElement("li");
+  li.innerText = this.format(times);
+  results.appendChild(li);
+}
 
-/* Stop button */
-stop.onclick = function() {
-  clearTimeout(t);
-};
+function clear() {
+  clearChildren(this.results);
+}
 
-/* Clear button */
-clear.onclick = function() {
-  h1.textContent = "00:00:00";
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
-};
+function step(timestamp) {
+  if (!this.running) return;
+  this.calculate(timestamp);
+  this.time = timestamp;
+  this.print();
+  requestAnimationFrame(this.step.bind(this));
+}
+
+function calculate(timestamp) {
+  var diff = timestamp - this.time;
+  // Hundredths of a second are 100 ms
+  this.times[2] += diff / 10;
+  // Seconds are 100 hundredths of a second
+  if (this.times[2] >= 100) {
+    this.times[1] += 1;
+    this.times[2] -= 100;
+  }
+  // Minutes are 60 `seconds
+  if (this.times[1] >= 60) {
+    this.times[0] += 1;
+    this.times[1] -= 60;
+  }
+}
+
+function print() {
+  display.innerText = format(times);
+}
+
+function format(times) {
+  return `\
+ ${pad0(times[0], 2)}:\
+ ${pad0(times[1], 2)}:\`
+ ${pad0(Math.floor(times[2]), 2)}`;
+}
+
+function pad0(value, count) {
+  var result = value + "";
+  for (; result.length < count; --count) result = "0" + result;
+  return result;
+}
+
+function clearChildren(node) {
+  while (node.lastChild) node.removeChild(node.lastChild);
+}
